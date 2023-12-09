7c4d275cdb6a27eef8c085a9df8f24da356468b4
Draven Troy <dravenpogicoloma@gmail.com>
2023-12-09 23:47:43 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DravenColoma-Block7-Quiz"
+include(":app")
